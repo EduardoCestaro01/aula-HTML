d785d538283bf7eaf987f27dfbfa17ea68bbf802
EduardoCestaro01 <eduardohenriquecestaro20@gmail.com>
2024-10-18 15:14:51 -0300

    tabuada

--- app.py
+++ app.py
@@ -47,3 +47,10 @@ def potencia(numero: float, elevado:float):
     return f""" 
     <h1> A potencia informada> N={numero}** E={elevado}  Potencia={numero**elevado}
     </h1>"""
+
+@app.route("/tabuada/<int:num>", methods=['GET'])
+def tabuada(num: int):
+  html="<ul>"
+  for i in range(1,11):
+    html+=f"<li>{num}x{i}={num*i}</li>"
+  return html + '</ul>'
